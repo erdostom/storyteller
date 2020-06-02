0007a1e309fb8ed6216d4467d0e59b49236a9339
Tamas Erdos <github@tamaserdos.com>
2020-06-02 14:47:10 -0400

    Updates

---
+++ precache-manifest.6aaf51e27146d8ae3f733fcf3650d339.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03dc1ac0f4d6d77e1ecc845444752cda",
+    "url": "/storyteller/index.html"
+  },
+  {
+    "revision": "4099391e71de1ea4e965",
+    "url": "/storyteller/static/css/main.e0e12ee4.chunk.css"
+  },
+  {
+    "revision": "8543786ee72bc57d38d6",
+    "url": "/storyteller/static/js/2.87f497ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/storyteller/static/js/2.87f497ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4099391e71de1ea4e965",
+    "url": "/storyteller/static/js/main.5ce052c9.chunk.js"
+  },
+  {
+    "revision": "c1aba4702f0ac3372c73",
+    "url": "/storyteller/static/js/runtime-main.1b45ad63.js"
+  }
+]);
